53287a86c6319cca1a8ab7aacee484c8f935db50
JuanGomez0303 <112434713+JuanGomez0303@users.noreply.github.com>
2025-08-23 16:32:05 -0500

    Estética del correo enviado

--- src/main/java/com/example/msauthentication/service/EmailService.java
+++ src/main/java/com/example/msauthentication/service/EmailService.java
@@ -1,27 +1,40 @@
 package com.example.msauthentication.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
-import java.security.SecureRandom;
 
 @Service
 public class EmailService {
 
-
-
     @Autowired
     private JavaMailSender mailSender;
 
-
+    @Autowired
+    private SpringTemplateEngine templateEngine;
 
     public void sendPasswordResetEmail(String to, String otp) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject("Solicitud de restablecimiento de contraseña");
-        message.setText("Esta es tu clave de un solo uso para restablecer tu contraseña: " + otp);
-        mailSender.send(message);
+
+        MimeMessage message = mailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setTo(to);
+            helper.setSubject("Solicitud de restablecimiento de contraseña");
+
+            Context context = new Context();
+            context.setVariable("otp", otp);
+            String htmlContent = templateEngine.process("PasswordReset", context);
+
+            helper.setText(htmlContent, true);
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
     }
+}
 }
